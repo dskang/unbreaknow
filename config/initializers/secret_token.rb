e46397eed87b977e7e0976771d3c9e812d7f1260
Dan Kang <dan@dskang.com>
2013-08-26 21:01:02 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unbreaknow::Application.config.secret_key_base = '9b111aac65295f8c1ecc02dc9669112f15f2a24b758e835ec3482eba7c0c470e7ed26e9732d1db6f22a9b4a1eb82776d6192a1e815f75ad9bd82d3084f85784f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Unbreaknow::Application.config.session_store :cookie_store, key: '_unbreaknow_session'
