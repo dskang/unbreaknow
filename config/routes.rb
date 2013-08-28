00c8fc4bfe9338026ae09e420dce8f1953ca4e16
Dan Kang <dan@dskang.com>
2013-08-27 21:53:25 -0700

    remove welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Unbreaknow::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'bugs#index'
 
-  resources :bugs, only: [:index, :new, :create, :show]
+  resources :bugs, only: [:new, :create, :show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
