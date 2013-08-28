d78eb0f804ee422d8f7c0a6c47205611eabb2117
Dan Kang <dan@dskang.com>
2013-08-28 03:14:32 -0700

    add more validations to bug model

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,3 +1,12 @@
 class Bug < ActiveRecord::Base
   validates :service_twitter, :short_description, presence: true
+  validates :service_twitter, length: { maximum: 50 }
+  validates :short_description, length: { maximum: 100 }
+  validate :service_twitter_must_be_single_word
+
+  def service_twitter_must_be_single_word
+    if service_twitter.split(" ").length > 1
+      errors.add(:service_twitter, "must be a single word")
+    end
+  end
 end
